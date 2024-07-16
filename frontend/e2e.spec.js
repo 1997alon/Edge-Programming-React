65925a0500095c8a9dc860d537852a4008e17649
yoavbd1997 <111271018+yoavbd1997@users.noreply.github.com>
2024-07-16 13:01:40 +0300

    tests/

---
+++ frontend/e2e.spec.js
@@ -0,0 +1,62 @@
+const { test, expect } = require('@playwright/test');
+
+test('theme', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+
+  await expect(page.locator('button.change_theme')).toBeVisible()
+});
+
+test('change theme', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+  
+    await expect(page.locator('button.change_theme')).toBeVisible();
+  
+    await page.click('button.change_theme');
+  
+    const backgroundColor = await page.evaluate(() => {
+      return window.getComputedStyle(document.body).backgroundColor;
+    });
+    expect(backgroundColor).toBe('rgba(0, 0, 0, 0)'); 
+  });
+
+
+  test('user', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+  
+    await expect(page.locator('button.create_user_form')).toBeVisible();
+  
+  });
+
+
+test('create user', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    await page.click('button.create_user_form');
+  
+    await page.fill('input[name="create_user_form_name"]', 'dd');
+    await page.fill('input[name="create_user_form_email"]', 'dd@dd.com');
+    await page.fill('input[name="create_user_form_username"]', 'dd');
+    await page.fill('input[name="create_user_form_password"]', 'dd');
+  
+    await page.click('button[name="create_user_form_create_user"]');
+    
+
+  });
+
+  test('login', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    await page.click('button.create_user_form');
+  
+    await page.fill('input[name="create_user_form_name"]', 'New User');
+    await page.fill('input[name="create_user_form_email"]', 'newuser@example.com');
+    await page.fill('input[name="create_user_form_username"]', 'newuser');
+    await page.fill('input[name="create_user_form_password"]', 'password123');
+  
+    await page.click('button[name="create_user_form_create_user"]');
+    await page.click('button.login_form');
+    await page.fill('input[name="login_form_username"]', 'newuser');
+    await page.fill('input[name="login_form_password"]', 'password123');
+    await page.click('button.login_form');
+    await expect(page.locator('button.logout')).toBeVisible();
+  });
