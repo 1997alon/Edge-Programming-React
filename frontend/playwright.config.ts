5a74bada19ecc8c38bdfa678f81c73ff5bf8e126
yoavbd1997 <111271018+yoavbd1997@users.noreply.github.com>
2024-07-22 12:12:44 +0300

    Create playwright.config.ts

---
+++ frontend/playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:3000',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
